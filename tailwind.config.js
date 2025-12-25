9137bc232c085a807dd75461bde4a1f90a8eaec9
cobrianz <briancheruiyot.h@gmail.com>
2025-12-25 14:51:21 +0300

    chore: Update Tailwind CSS content paths to include the `src` directory.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
@@ -21,7 +21,7 @@ module.exports = {
           "scrollbar-width": "none",
           /* Hide scrollbar for IE and Edge */
           "-ms-overflow-style": "none",
-    
+
         },
       });
     },
